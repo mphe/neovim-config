8adafeca63abc8a7791f79dbd25b934cf60d6bd2
Marvin Ewald <marvin.e@protonmail.com>
2026-02-13 16:24:22 +0100

    Add workaround to not break blink completion removal with automatic line breaks

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -181,3 +181,35 @@ local custom_capabilities = {}
 vim.lsp.config("*", {
     capabilities = require("blink.cmp").get_lsp_capabilities(custom_capabilities)
 })
+
+
+-- Disable automatic line break while blink menu is open to prevent breaking text replacement
+-- https://github.com/saghen/blink.cmp/issues/1445#issuecomment-3512124299
+vim.api.nvim_create_autocmd("User", {
+    pattern = "BlinkCmpMenuOpen",
+    callback = function()
+        local formatoptions = vim.opt.formatoptions:get()
+        if formatoptions.t then
+            vim.b.restore_formatoptions_t = true
+            vim.opt.formatoptions:remove("t")
+        end
+        if formatoptions.c then
+            vim.b.restore_formatoptions_c = true
+            vim.opt.formatoptions:remove("c")
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "BlinkCmpMenuClose",
+    callback = function()
+        if vim.b.restore_formatoptions_t then
+            vim.opt.formatoptions:append("t")
+            vim.b.restore_formatoptions_t = nil
+        end
+        if vim.b.restore_formatoptions_c then
+            vim.opt.formatoptions:append("c")
+            vim.b.restore_formatoptions_c = nil
+        end
+    end,
+})
