25f6c62c53502006f90cec64d7e6f1178e3a8456
Marvin Ewald <marvin.e@protonmail.com>
2026-03-21 21:43:50 +0100

    Tweak diagnostic display
    - Update LSP diagnostic display to Nvim 0.12
    - Remap hint diagnostics to info diagnostic on LSP servers that use
      hints as infos.

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,41 +1,52 @@
 local M = {}
 
--- Show full diagnostic information
--- https://github.com/neovim/neovim/issues/19649#issuecomment-1750272564
-local publishDiagnosticsOriginal = vim.lsp.handlers['textDocument/publishDiagnostics']
 
-local function publishDiagnosticsCustom(_, result, ctx, config)
+-- Add related information as additional diagnostics
+-- https://github.com/neovim/neovim/issues/19649#issuecomment-1750272564
+local function show_related_information(diagnostics)
     vim.tbl_map(function(item)
         if item.relatedInformation and #item.relatedInformation > 0 then
             vim.tbl_map(function(k)
                 if k.location then
                     local tail = vim.fn.fnamemodify(vim.uri_to_fname(k.location.uri), ':t')
-                    k.message = tail ..
-                    '(' .. (k.location.range.start.line + 1) .. ', ' .. (k.location.range.start.character + 1) ..
-                    '): ' .. k.message
+                    k.message = string.format("%s(%d,%d): %s", tail, k.location.range.start.line + 1, k.location.range.start.character + 1, k.message)
 
-               if k.location.uri == vim.uri_from_bufnr(0) then
-                  table.insert(result.diagnostics, {
-                     code = item.code,
-                     message = k.message,
-                     range = k.location.range,
-                     severity = vim.lsp.protocol.DiagnosticSeverity.Hint,
-                     source = item.source,
-                     relatedInformation = {},
-                  })
-               end
-            end
-            -- -- We could append it to the main message here or we can do it in the diagnostic format handler.
-            -- item.message = item.message .. '\n' .. k.message
-            item.message = item.message
-         end, item.relatedInformation)
-      end
-   end, result.diagnostics)
-   publishDiagnosticsOriginal(_, result, ctx, config)
+                    if k.location.uri == vim.uri_from_bufnr(0) then
+                        table.insert(diagnostics, {
+                            code = item.code,
+                            message = k.message,
+                            range = k.location.range,
+                            severity = vim.lsp.protocol.DiagnosticSeverity.Hint,
+                            source = item.source,
+                            relatedInformation = {},
+                        })
+                    end
+                end
+
+                -- -- We could append it to the main message here or we can do it in the diagnostic format handler.
+                -- item.message = item.message .. '\n' .. k.message
+            end, item.relatedInformation)
+        end
+    end, diagnostics)
 end
 
+local function remap_severities(diagnostics)
+    vim.tbl_map(function(item)
+        if item.source ~= "clang" then
+            if item.severity == vim.diagnostic.severity.HINT then
+                item.severity = vim.diagnostic.severity.INFO
+            end
+        end
+    end, diagnostics)
+end
 
-vim.lsp.handlers['textDocument/publishDiagnostics'] = publishDiagnosticsCustom
+local publishDiagnosticsOriginal = vim.lsp.handlers['textDocument/publishDiagnostics']
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] = function(_, result, ctx, config)
+    -- show_related_information(result.diagnostics)
+    remap_severities(result.diagnostics)
+    return publishDiagnosticsOriginal(_, result, ctx, config)
+end
 
 vim.diagnostic.config({
     signs = {
@@ -48,23 +59,35 @@ vim.diagnostic.config({
         priority = 100,
     },
     float = {
-        -- Use custom suffix and format handlers to show URL references and related information in
-        -- diagnostic messages. Supported by LSP spec, but not by Nvim by default.
+        source = true,
+
         format = function(diagnostic)
             local msg = diagnostic.message
             local lsp_data = diagnostic.user_data and diagnostic.user_data.lsp
 
-            if lsp_data and lsp_data.relatedInformation then
-                local infos = vim.tbl_map(function(info)
-                    return info.message
-                end, lsp_data.relatedInformation)
-                if #infos > 0 then
-                    msg = string.format("%s\n⮡  %s", msg, table.concat(infos, "\n⮡  "))
+            -- Related information in diagnostic messages is only supported since Nvim 0.12.
+            if not vim.fn.has("nvim-0.12") then
+                if lsp_data and lsp_data.relatedInformation then
+                    local infos = vim.tbl_map(function(info)
+                        return info.message
+                    end, lsp_data.relatedInformation)
+                    if #infos > 0 then
+                        msg = string.format("%s\n⮡  %s", msg, table.concat(infos, "\n⮡  "))
+                    end
                 end
             end
 
-            return msg .. "\n"  -- Force suffix on new line
+            -- Force suffix on new line only when it will be rendered as link. This prevents weird
+            -- highlighting with underlines if it causes a line break.
+            if lsp_data and lsp_data.codeDescription and lsp_data.codeDescription.href then
+                -- msg = string.format("%s\n⮡  [More info](%s)", msg, lsp_data.codeDescription.href)
+                msg = msg .. "\n"  -- Force suffix on new line
+            end
+
+            return msg
         end,
+
+        -- Display diagnostic icons instead of numbers
         prefix = function(diagnostic)
             local hl = {
                 [vim.diagnostic.severity.ERROR] = "DiagnosticError",
@@ -74,15 +97,20 @@ vim.diagnostic.config({
             }
             return string.format("%s ", vim.diagnostic.config().signs.text[diagnostic.severity] or ""), hl[diagnostic.severity] or ""
         end,
+
+        -- Append the diagnostic code and format it as link to the documentation URL if available
         suffix = function(diagnostic)
             local code = diagnostic.code
             local lsp_data = diagnostic.user_data and diagnostic.user_data.lsp
 
-            if code and lsp_data and lsp_data.codeDescription and lsp_data.codeDescription.href then
-                return string.format("[%s](%s)", code, lsp_data.codeDescription.href), "markdownLinkText"
-            elseif code then
-                return string.format("[%s]", code)
+            if code then
+                if lsp_data and lsp_data.codeDescription and lsp_data.codeDescription.href then
+                    return string.format("[%s](%s)", code, lsp_data.codeDescription.href), "markdownLinkText"
+                else
+                    return string.format(" [[%s]]", code)
+                end
             end
+
             return ""
         end,
     },
